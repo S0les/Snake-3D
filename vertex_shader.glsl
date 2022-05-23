060bab2455c95539081473c06ea9c0b28652f70a
S0les <bitfun6@gmail.com>
2022-05-24 01:07:59 +0200

    [#11] main_file: Add textures loading
    Adds functions to load texture
    
    Adds texture processing, but as output got black screen

--- vertex_shader.glsl
+++ vertex_shader.glsl
@@ -1,11 +1,14 @@
 #version 330 core
 
 layout (location = 0) in vec3 position;
+layout (location = 1) in vec2 textureCoords;
 
 out vec3 ourColor;
+out vec2 TexCoord;
 
 void main()
 {
     gl_Position = vec4(position.x, position.y, position.z, 1.0);
     ourColor = vec3(0.5, 0.01, 0.5);
+    TexCoord = textureCoords;
 }
